f9b2279198758721fb42a9277f91074181127aa1
luciano <luchogalli@gmail.com>
2023-10-21 15:39:51 -0300

    ojo no esta tan mal

--- src/assets/components/ProductDetail.jsx
+++ src/assets/components/ProductDetail.jsx
@@ -1,26 +1,29 @@
-  import React from 'react';
-  import { useParams, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import './ProductDetail.css';
+
+const ProductDetail = ({ products }) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const product = products.find((p) => p.id === parseInt(id, 10));
   
-  const ProductDetail = ({ products }) => {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const product = products.find((p) => p.id === parseInt(id, 10));
-    const GoBack = () => {
-      navigate('/Catalogo')
-    }
-  
-    if (!product) {
-      return <div>Producto no encontrado.</div>;
-    }
-  
-    return (
-      <div>
-        <h2>Detalle del Producto</h2>
-        <h3>{product.name}</h3>
-        <p>{product.description}</p>
-        <button onClick={GoBack}>Go Back</button>
-      </div>
-    );
-  };
-  
-  export default ProductDetail;
+  const GoBack = () => {
+    navigate('/Catalogo')
+  }
+
+  if (!product) {
+    return <div>Producto no encontrado.</div>;
+  }
+
+  return (
+    <div>
+      <h2>Detalle del Producto</h2>
+      <h3>{product.name}</h3>
+      <img src={product.image} alt={product.name} />
+      <p>{product.description}</p>
+      <button onClick={GoBack}>Go Back</button>
+    </div>
+  );
+};
+
+export default ProductDetail;
--- src/assets/components/Productos.jsx
+++ src/assets/components/Productos.jsx
@@ -1,15 +1,14 @@
-
 export const products = [
   {
     id: 1,
     name: "Producto 1",
-    description: "Descripción del Producto 1",
-
+    description: "BMW wow",
+    image: "https://prod.cosy.bmw.cloud/bmwweb/cosySec?COSY-EU-100-2545xM4RIyFnbm9Mb3AgyyIJrjG0suyJRBODlsrjGpuaprQbhSIqppglBg7mQJl384MlficYiGHqoQxYLW7%25f3tiJ0PCJirQbLDWcQW7%251uRCRqoQh47wMvcYi9k%25rfMb3islBglUbtzcRScH8e7MbnMdo13yJGy536UrQ%25r9RX9W8zWuEUHqogqaFH8l3ilUj8ocRScH705MbnMd0ViyJGy5iubrQ%25r9SbUW8zWunJjqogqaGQFl3ilU%25zocRScHzeQMbnMdg%25zyJGy5iJUrQ%25r9SejW8zWuKGEqogqaDC8l3ilUCQQcRScH48gMbnMdezOyJGy5QoarQ%25r98nDW8zWuoYEqogqa3J%25l3ilURQacRScHb8%25MbnMdJu3yJGy5QiErQ%25r98StW8zWuo8hqogqa3s7l3ilURKjcRScHHQ9MbnMdd8syJGy558arQ%25r99uDW8JYHltW7%25XlWrQb9%25cQSW7%25IER8MI8YHQA",
   },
   {
     id: 2,
     name: "Producto 2",
-    description: "Descripción del Producto 2",
-
+    description: "Descripción detallada del Producto 2",
+    image: "https://www.google.com/search?q=bmw&tbm=isch&ved=2ahUKEwiQ3fe_4YeCAxVaqZUCHVc-BMEQ2-cCegQIABAA&oq=bmw&gs_lcp=CgNpbWcQAzIHCAAQigUQQzIKCAAQigUQsQMQQzIHCAAQigUQQzINCAAQigUQsQMQgwEQQzIKCAAQigUQsQMQQzIKCAAQigUQsQMQQzIKCAAQigUQsQMQQzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzoECCMQJzoHCCMQ6gIQJzoFCAAQgAQ6CwgAEIAEELEDEIMBUPUWWJQvYKs0aAFwAHgAgAFBiAH8AZIBATSYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABCsABAQ&sclient=img&ei=Qxc0ZdCvL9rS1sQP1_yQiAw&bih=963&biw=1920#imgrc=EWsDYCX6oPWWBM",
   },
 ];
