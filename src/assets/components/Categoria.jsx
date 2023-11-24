0c61783d09c26757314fd1b6fff60a072bdacf45
luchogalli <36230321+luchogalli@users.noreply.github.com>
2023-11-23 21:16:24 -0300

    firebase test

--- src/assets/components/Categoria.jsx
+++ src/assets/components/Categoria.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { categorias }  from '../data/Categorias.js';
 import './Categoria.css';
-
+import { doc, getDoc, getFirestore } from "firebase/firestore"
 const Categoria = ({ products }) => {
+    useEffect(() => {
+        const db = getFirestore();
+    
+        const biciRef = doc(db, "categorias", "X");
+        getDoc(biciRef).then(snapshot => {
+            if (snapshot.exists()) {
+                console.log(snapshot.data());
+             //   setProduct({ id: snapshot.id, ...snapshot.data() });
+            }
+        });
+    }, []);
   return (
     <div className="catalog-container">
       <h2>Categorias de Productos</h2>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDitehZojuHNsXtBQLl-0qihNpoLsIzVZA",
+  authDomain: "coder-bmw.firebaseapp.com",
+  projectId: "coder-bmw",
+  storageBucket: "coder-bmw.appspot.com",
+  messagingSenderId: "1052753081538",
+  appId: "1:1052753081538:web:a1b7547a208baf858e775b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>,
+
 )
