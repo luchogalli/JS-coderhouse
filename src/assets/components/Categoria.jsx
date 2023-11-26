9321f8c6c8f781f6a923330ec51fb5a2e212e467
luchogalli <36230321+luchogalli@users.noreply.github.com>
2023-11-26 19:41:33 -0300

    ctegories ok

--- src/assets/components/Categoria.jsx
+++ src/assets/components/Categoria.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { categorias }  from '../data/Categorias.js';
 import './Categoria.css';
-import { doc, getDoc, getFirestore, collection, getDocs } from "firebase/firestore"
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const Categoria = ({ products }) => {
-    useEffect(() => {
-      async function FetchCategories(){
-        const db = getFirestore();
-    
-        const coleccionFB = collection(db, "categorias");
-        const Docus = await getDocs (coleccionFB);
-        const listDocus = Docus.docs.map(doc => doc.data());
-        console.log(listDocus);
-      }
-      FetchCategories();
-    }, []);
+  const [listDocus, setListDocus] = useState([]); 
+
+  useEffect(() => {
+    async function FetchCategories() {
+      const db = getFirestore();
+
+      const coleccionFB = collection(db, "categorias");
+      const Docus = await getDocs(coleccionFB);
+      const categoriesData = Docus.docs.map(doc => {
+        const data = doc.data();
+        return { ...data, id: doc.id };
+      });
+      setListDocus(categoriesData); 
+    }
+    FetchCategories();
+  }, []);
 
   return (
     <div className="catalog-container">
       <h2>Categorias de Productos</h2>
       <ul className="product-list">
-        {categorias.map((categoria) => (
+        {listDocus.map((categoria) => (
           <li key={categoria.id} className="product-item">
             <Link to={`/Categoria/${categoria.id}`} className="product-link">
               <img src={categoria.image} alt={categoria.name} className="product-image" />
